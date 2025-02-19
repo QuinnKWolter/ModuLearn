ea65c49cfcdf00ac9f4fa286835817b667f55c85
Mohammad Hassany <moh70@pitt.edu>
2025-02-19 13:01:14 -0500

    update docker configs

--- docker/reload-from-github.sh
+++ docker/reload-from-github.sh
@@ -1,7 +1,7 @@
 # put this file outside the modulearn folder
 # then use it to update the modulearn and rebuild/run the docker containers
-sudo rm -rf modulearn
+sudo rm -rf ModuLearn
 git clone https://github.com/QuinnKWolter/ModuLearn.git
-sudo docker-compose --env-file .env.host-ip build --no-cache
-sudo docker-compose --env-file .env.host-ip down
-sudo docker-compose --env-file .env.host-ip up -d
+sudo docker-compose build --no-cache
+sudo docker-compose down
+sudo docker-compose up -d
