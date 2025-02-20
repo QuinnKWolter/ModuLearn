46a42de478174d6b8df602c2686811d80bf95b8b
Quinn K Wolter <QuinnKWolter@gmail.com>
2025-02-20 11:41:59 -0500

    Docker troubleshooting 05

--- modulearn/modulearn/urls.py
+++ modulearn/modulearn/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Admin site
@@ -19,4 +21,4 @@ urlpatterns = [
 
     # Static Pages
     path('', include('main.urls', namespace='main')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
