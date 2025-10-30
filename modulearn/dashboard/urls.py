ef31d97acaf4f5be08b496f112997ac913d23583
Quinn K Wolter <QuinnKWolter@gmail.com>
2025-10-30 11:15:10 -0400

    Ready for initial review.

--- modulearn/dashboard/urls.py
+++ modulearn/dashboard/urls.py
@@ -6,7 +6,8 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('student/', views.student_dashboard, name='student_dashboard'),
-    path('instructor/', views.instructor_dashboard, name='instructor_dashboard'),
+    # TEMP: route instructor dashboard to mockup; revert to views.instructor_dashboard when done
+    path('instructor/', views.mockup_dashboard, name='instructor_dashboard'),
     path('mockup/', views.mockup_dashboard, name='mockup_dashboard'),
     path("api/generate_course_auth_url/", generate_course_auth_url, name="generate_course_auth_url"),
     path("api/fetch_analytics_data/", views.fetch_analytics_data, name="fetch_analytics_data"),
